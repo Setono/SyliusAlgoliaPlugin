bbaad5592bab750a0c93357c297223c4840a1d20
Joachim Løvgaard <joachim@loevgaard.dk>
2022-04-20 08:20:29 +0200

    Fix service ids and use priority trait from Symfony

--- src/DependencyInjection/Compiler/RegisterUrlGeneratorsPass.php
+++ src/DependencyInjection/Compiler/RegisterUrlGeneratorsPass.php
@@ -5,22 +5,23 @@ declare(strict_types=1);
 namespace Setono\SyliusAlgoliaPlugin\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Compiler\PriorityTaggedServiceTrait;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Reference;
 
 final class RegisterUrlGeneratorsPass implements CompilerPassInterface
 {
+    use PriorityTaggedServiceTrait;
+
     public function process(ContainerBuilder $container): void
     {
-        if (!$container->has('setono_sylius_algolia.url_generator.composite_resource')) {
+        if (!$container->has('setono_sylius_algolia.url_generator.composite')) {
             return;
         }
 
-        $composite = $container->getDefinition('setono_sylius_algolia.url_generator.composite_resource');
+        $composite = $container->getDefinition('setono_sylius_algolia.url_generator.composite');
 
-        /** @var string $id */
-        foreach (array_keys($container->findTaggedServiceIds('setono_sylius_algolia.url_generator')) as $id) {
-            $composite->addMethodCall('add', [new Reference($id)]);
+        foreach ($this->findAndSortTaggedServices('setono_sylius_algolia.url_generator', $container) as $service) {
+            $composite->addMethodCall('add', [$service]);
         }
     }
 }
