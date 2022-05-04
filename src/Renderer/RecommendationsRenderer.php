5da337c2c87a8e19068a7329f57108befc2cd94c
Joachim Løvgaard <joachim@loevgaard.dk>
2022-05-04 11:05:07 +0200

    Added logging to RecommendationsRenderer

--- src/Renderer/RecommendationsRenderer.php
+++ src/Renderer/RecommendationsRenderer.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 
 namespace Setono\SyliusAlgoliaPlugin\Renderer;
 
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 use Setono\SyliusAlgoliaPlugin\Provider\Recommendations\RecommendationsProviderInterface;
 use Sylius\Component\Core\Model\ProductInterface;
 use Twig\Environment;
 
-final class RecommendationsRenderer implements RecommendationsRendererInterface
+final class RecommendationsRenderer implements RecommendationsRendererInterface, LoggerAwareInterface
 {
+    private LoggerInterface $logger;
+
     private RecommendationsProviderInterface $recommendationsProvider;
 
     private Environment $twig;
@@ -21,6 +26,7 @@ final class RecommendationsRenderer implements RecommendationsRendererInterface
         Environment $twig,
         bool $debug
     ) {
+        $this->logger = new NullLogger();
         $this->recommendationsProvider = $recommendationsProvider;
         $this->twig = $twig;
         $this->debug = $debug;
@@ -35,6 +41,13 @@ final class RecommendationsRenderer implements RecommendationsRendererInterface
         } catch (\Throwable $e) {
             $products = [];
             $error = $e->getMessage();
+
+            $this->logger->error(sprintf(
+                "An error occurred while trying to fetch recommended products for product with code '%s'. The error was: %s. The trace was:\n\n%s\n",
+                (string) $product->getCode(),
+                $e->getMessage(),
+                $e->getTraceAsString()
+            ));
         }
 
         return $this->twig->render('@SetonoSyliusAlgoliaPlugin/shop/product/_frequently_bought_together.html.twig', [
@@ -43,5 +56,10 @@ final class RecommendationsRenderer implements RecommendationsRendererInterface
             'error' => $error,
             'debug' => $this->debug,
         ]);
+    }
+
+    public function setLogger(LoggerInterface $logger): void
+    {
+        $this->logger = $logger;
     }
 }
