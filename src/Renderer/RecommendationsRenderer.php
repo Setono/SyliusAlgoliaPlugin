1cac08c8d7f454795c8e647827f6bbedcc63ef6d
Joachim Løvgaard <joachim@loevgaard.dk>
2022-05-04 10:58:31 +0200

    Simplified RecommendationsRenderer

--- src/Renderer/RecommendationsRenderer.php
+++ src/Renderer/RecommendationsRenderer.php
@@ -16,8 +16,11 @@ final class RecommendationsRenderer implements RecommendationsRendererInterface
 
     private bool $debug;
 
-    public function __construct(RecommendationsProviderInterface $recommendationsProvider, Environment $twig, bool $debug)
-    {
+    public function __construct(
+        RecommendationsProviderInterface $recommendationsProvider,
+        Environment $twig,
+        bool $debug
+    ) {
         $this->recommendationsProvider = $recommendationsProvider;
         $this->twig = $twig;
         $this->debug = $debug;
@@ -25,17 +28,20 @@ final class RecommendationsRenderer implements RecommendationsRendererInterface
 
     public function renderFrequentlyBoughtTogether(ProductInterface $product, string $index, int $max = 10): string
     {
+        $error = null;
+
         try {
-            return $this->twig->render('@SetonoSyliusAlgoliaPlugin/shop/product/_frequently_bought_together.html.twig', [
-                'product' => $product,
-                'products' => [...$this->recommendationsProvider->getFrequentlyBoughtTogether($product, $index, $max)],
-            ]);
+            $products = [...$this->recommendationsProvider->getFrequentlyBoughtTogether($product, $index, $max)];
         } catch (\Throwable $e) {
-            return $this->twig->render('@SetonoSyliusAlgoliaPlugin/shop/product/frequently_bought_together/error.html.twig', [
-                'product' => $product,
-                'error' => $e->getMessage(),
-                'debug' => $this->debug,
-            ]);
+            $products = [];
+            $error = $e->getMessage();
         }
+
+        return $this->twig->render('@SetonoSyliusAlgoliaPlugin/shop/product/_frequently_bought_together.html.twig', [
+            'product' => $product,
+            'products' => $products,
+            'error' => $error,
+            'debug' => $this->debug,
+        ]);
     }
 }
