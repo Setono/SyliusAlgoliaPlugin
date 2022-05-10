51a94420cfe7330208b634eff7de85bd0470ac48
Joachim Løvgaard <joachim@loevgaard.dk>
2022-05-10 08:54:31 +0200

    Simplified methods

--- src/Provider/Recommendations/RecommendationsProvider.php
+++ src/Provider/Recommendations/RecommendationsProvider.php
@@ -8,6 +8,7 @@ use Doctrine\Persistence\ManagerRegistry;
 use Setono\DoctrineObjectManagerTrait\ORM\ORMManagerTrait;
 use Setono\SyliusAlgoliaPlugin\Client\RecommendationsClient\RecommendationsClientInterface;
 use Setono\SyliusAlgoliaPlugin\Config\IndexableResourceCollection;
+use Setono\SyliusAlgoliaPlugin\Document\Document;
 use Setono\SyliusAlgoliaPlugin\Model\ObjectIdAwareInterface;
 use Sylius\Component\Core\Model\ProductInterface;
 use Webmozart\Assert\Assert;
@@ -34,32 +35,22 @@ final class RecommendationsProvider implements RecommendationsProviderInterface
     {
         Assert::isInstanceOf($product, ObjectIdAwareInterface::class);
 
-        $documents = $this->recommendationsClient->getFrequentlyBoughtTogether($product, $index, $max);
-
-        foreach ($documents as $document) {
-            Assert::notNull($document->resourceName);
-
-            $indexableResource = $this->indexableResourceCollection->getByName($document->resourceName);
-
-            $repository = $this->getRepository($indexableResource->className);
-            $entity = $repository->findOneBy([
-                'code' => $document->code,
-            ]);
-
-            Assert::nullOrIsInstanceOf($entity, ProductInterface::class);
-
-            if (null !== $entity) {
-                yield $entity;
-            }
-        }
+        return $this->getRecommendations($this->recommendationsClient->getFrequentlyBoughtTogether($product, $index, $max));
     }
 
     public function getRelatedProducts(ProductInterface $product, string $index, int $max = 10): \Generator
     {
         Assert::isInstanceOf($product, ObjectIdAwareInterface::class);
 
-        $documents = $this->recommendationsClient->getRelatedProducts($product, $index, $max);
+        return $this->getRecommendations($this->recommendationsClient->getRelatedProducts($product, $index, $max));
+    }
 
+    /**
+     * @param iterable<Document> $documents
+     * @return \Generator<ProductInterface>
+     */
+    private function getRecommendations(iterable $documents): \Generator
+    {
         foreach ($documents as $document) {
             Assert::notNull($document->resourceName);
 
